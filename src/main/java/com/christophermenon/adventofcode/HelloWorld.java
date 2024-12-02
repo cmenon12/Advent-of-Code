87bcd840d01412aa26d1f1718b27559abfd1fc85
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2024-12-02 15:47:09 +0000

    Create maven project with HelloWorld

---
+++ src/main/java/com/christophermenon/adventofcode/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.christophermenon.adventofcode;
+
+/**
+ *
+ * @author cmenon12
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
