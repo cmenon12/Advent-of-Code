2523a8b2bd0f4fa33f93552dc48b7c8fdf6cb0ed
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2024-12-05 15:00:38 +0000

    Complete part 2 for day 5

--- src/main/java/com/christophermenon/adventofcode/year2024/Day05.java
+++ src/main/java/com/christophermenon/adventofcode/year2024/Day05.java
@@ -3,6 +3,7 @@ package com.christophermenon.adventofcode.year2024;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.christophermenon.adventofcode.PuzzleInput;
 
@@ -23,7 +24,8 @@ public class Day05 {
             rulesMap.get(key).add(val);
         }
 
-        int total = 0;
+        int part1Total = 0;
+        int part2Total = 0;
         for (String book: books) {
             
             // Get a list of book numbers
@@ -47,10 +49,47 @@ public class Day05 {
                 }
             }
 
-            // If valid then save the middle value in the book
-            if (validBook) total += bookNumbers.get(Math.floorDiv(bookNumbers.size(), 2));
+            // If valid then save the middle value in the book for part 1
+            if (validBook) {
+                part1Total += bookNumbers.get(Math.floorDiv(bookNumbers.size(), 2));
+            // Otherwise reorder and save for part 2
+            } else {
+                part2Total += reorderAndGetTotal(rulesMap, bookNumbers);
+            }
         }
 
-        System.out.printf("PART 1: %d%n", total);
+        System.out.printf("PART 1: %d%n", part1Total);
+        System.out.printf("PART 2: %d%n", part2Total);
+    }
+
+    private static int reorderAndGetTotal(HashMap<Integer, ArrayList<Integer>> rulesMap, ArrayList<Integer> bookNumbers) {
+
+        // For each entry in the rules
+        for (Map.Entry<Integer, ArrayList<Integer>> rule : rulesMap.entrySet()) {
+            Integer ruleKey = rule.getKey();
+            ArrayList<Integer> ruleValues = rule.getValue();
+
+            // If the rule key is in the book
+            if (bookNumbers.contains(ruleKey)) {
+                List<Integer> bookNumbersSubset = bookNumbers.subList(0, bookNumbers.indexOf(ruleKey));
+
+                // Find the earliest index where an offending rule value occurs
+                int earliestIndex = -1;
+                for (Integer ruleVal : ruleValues) {
+                    int ruleValIndex = bookNumbersSubset.indexOf(ruleVal);
+                    if ((ruleValIndex < earliestIndex || earliestIndex == -1) && ruleValIndex != -1) {
+                        earliestIndex = ruleValIndex;
+                    }
+                }
+
+                // Move the rule key to just before the earliest offending value
+                if (earliestIndex != -1) {
+                    bookNumbers.remove(ruleKey);
+                    bookNumbers.add(earliestIndex, ruleKey);
+                }
+            }
+        }
+        
+        return bookNumbers.get(Math.floorDiv(bookNumbers.size(), 2));
     }
 }
