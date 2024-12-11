3b9e3ee311973d9abe226e66413ea49ae529d817
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2024-12-11 22:15:53 +0000

    Complete part 2 for day 9

--- src/main/java/com/christophermenon/adventofcode/year2024/Day09.java
+++ src/main/java/com/christophermenon/adventofcode/year2024/Day09.java
@@ -8,18 +8,7 @@ public class Day09 {
 
     public static void main(String[] args) {
         String puzzle = PuzzleInput.getAsString(2024, 9);
-
-        // Create and fill the disk map
-        ArrayList<Integer> diskMap = new ArrayList<>();
-        for (int i = 0; i < puzzle.length()-1; i++) {
-            int val = Integer.parseInt(puzzle.substring(i, i+1));
-            if (i % 2 == 0) {
-                if (val == 0) System.err.println("we're trying to add a file that's zero blocks long!");
-                addToDiskMap(diskMap, i / 2, val);
-            } else {
-                addToDiskMap(diskMap, null, val);
-            }
-        }
+        ArrayList<Integer> diskMap = createDiskMap(puzzle);
 
         // Apply compacting
         while (!isCompactingComplete(diskMap)) {
@@ -29,7 +18,7 @@ public class Day09 {
             diskMap.set(lastValIndex, null);
         }
 
-        // Calculate the total
+        // Calculate the part 1 total
         long total = 0;
         for (int i = 0; i < diskMap.size(); i++) {
             Integer val = diskMap.get(i);
@@ -37,19 +26,75 @@ public class Day09 {
             total += i * val;
         }
 
+        // Complete part 1
         System.out.printf("PART 1: %d%n", total);
+
+        // Apply compacting for part 2
+        diskMap = createDiskMap(puzzle);
+        int lastVal = (puzzle.length() / 2) - 1;
+        for (int val = lastVal; val >= 0; val--) {
+            int valLength = diskMap.lastIndexOf(val) - diskMap.indexOf(val) + 1;
+            int nullIndex = getEarliestIndexOfNulls(diskMap, valLength);
+            if (nullIndex != -1 && nullIndex < diskMap.lastIndexOf(val)) {
+                while (diskMap.indexOf(val) != -1) {
+                    diskMap.set(diskMap.indexOf(val), null);
+                }
+                for (int i = nullIndex; i < nullIndex+valLength; i++) {
+                    diskMap.set(i, val);
+                }
+            }
+        }
+
+        // Calculate the part 2 total
+        total = 0;
+        for (int i = 0; i < diskMap.size(); i++) {
+            Integer val = diskMap.get(i);
+            if (val == null) continue;
+            total += i * val;
+        }
+
+        // Complete part 2
+        System.out.printf("PART 2: %d%n", total);
+
 
     }
 
-    private static void addToDiskMap(ArrayList<Integer> diskMap, Integer val, int count) {
-        for (int i = 0; i < count; i++) {
-            diskMap.add(val);
+    private static ArrayList<Integer> createDiskMap(String puzzle) {
+        ArrayList<Integer> diskMap = new ArrayList<>();
+        for (int i = 0; i < puzzle.length()-1; i++) {
+            int val = Integer.parseInt(puzzle.substring(i, i+1));
+            if (i % 2 == 0) {
+                if (val == 0) System.err.println("we're trying to add a file that's zero blocks long!");
+                for (int j = 0; j < val; j++) {
+                    diskMap.add(i / 2);
+                }
+            } else {
+                for (int j = 0; j < val; j++) {
+                    diskMap.add(null);
+                }
+            }
         }
+        return diskMap;
     }
 
     private static int getIndexOfLastVal(ArrayList<Integer> diskMap) {
         for (int i = diskMap.size() - 1; i >= 0; i--) {
             if (diskMap.get(i) != null) return i;
+        }
+        return -1;
+    }
+
+    private static int getEarliestIndexOfNulls(ArrayList<Integer> diskMap, int targetLength) {
+        int currentLength = 0;
+        for (int i = 0; i < diskMap.size(); i++) {
+            if (diskMap.get(i) == null) {
+                currentLength += 1;
+                if (currentLength == targetLength) {
+                    return i - targetLength + 1;
+                }
+            } else {
+                currentLength = 0;
+            }
         }
         return -1;
     }
