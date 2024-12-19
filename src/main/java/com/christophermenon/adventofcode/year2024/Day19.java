0164b506578bcf3db6fe00903fb75239f525fb46
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2024-12-19 11:04:11 +0000

    Complete part 2 for day 19

--- src/main/java/com/christophermenon/adventofcode/year2024/Day19.java
+++ src/main/java/com/christophermenon/adventofcode/year2024/Day19.java
@@ -2,6 +2,7 @@ package com.christophermenon.adventofcode.year2024;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import com.christophermenon.adventofcode.PuzzleInput;
 
@@ -15,36 +16,67 @@ public class Day19 {
         towels.addAll(Arrays.asList(puzzle.get(0).split(", ")));
         ArrayList<String> patterns = new ArrayList<>(puzzle.subList(2, puzzle.size()));
 
-        // Try to create each pattern recursively
-        int total = 0;
-        for (String pattern: patterns) {
-            if (createPatternFromTowels(pattern, towels)) total += 1;
-        }
+        // Create a solution object and solve it
+        Solution solution = new Solution(towels);
+        solution.solveAll(patterns);
+    }
+}
 
-        // Complete part 1
-        System.out.printf("PART 1: %d%n", total);
+class Solution {
+
+    ArrayList<String> towels;
+    HashMap<String, Long> preSolvedPatterns;
+
+    // Create the variables we want across the solution methods
+    Solution(ArrayList<String> towels) {
+        this.towels = towels;
+        this.preSolvedPatterns = new HashMap<>();
     }
 
-    private static boolean createPatternFromTowels(String pattern, ArrayList<String> towels) {
+    void solveAll(ArrayList<String> patterns) {
+        long part1Total = 0;
+        long part2Total = 0;
+
+        // Solve each pattern
+        for (String pattern: patterns) {
+            long total = createPatternFromTowels(pattern);
+            if (total > 0) part1Total += 1;
+            part2Total += total;
+        }
+
+        // Complete both parts
+        System.out.printf("PART 1: %d%n", part1Total);
+        System.out.printf("PART 2: %d%n", part2Total);
+    }
+
+    long createPatternFromTowels(String pattern) {
         
         // Break out if we've completed the pattern
         if (pattern.isEmpty()) {
-            return true;
+            return 1;
         }
 
         // Try with each towel
+        long allTotals = 0;
         for (String t : towels) {
 
             // If the towel is not too big
             // And the start of the pattern equals the towel
-            // And recrusively following this route completes the pattern
-            // Then return true
             if (t.length() <= pattern.length() && 
-            t.equals(pattern.substring(0, t.length())) && 
-            createPatternFromTowels(pattern.substring(t.length()), towels)) return true;
+            t.equals(pattern.substring(0, t.length()))) {
+
+                // If we don't already have the total for the next pattern
+                // Then recursively solve it and save it
+                if (!preSolvedPatterns.containsKey(pattern.substring(t.length()))) {
+                    preSolvedPatterns.put(pattern.substring(t.length()), createPatternFromTowels(pattern.substring(t.length())));
+                }
+
+                // Add the total for the next pattern
+                allTotals += preSolvedPatterns.get(pattern.substring(t.length()));
+            }
         }
 
-        // None of the towels could complete the pattern
-        return false;
+        // Return the total number of solutions
+        return allTotals;
     }
 }
